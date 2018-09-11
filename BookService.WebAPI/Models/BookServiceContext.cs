adb8449afd674137e3cfce180f5bb0139ed147b2
wilschok <william.schokkele@howest.be>
2018-09-11 13:19:44 +0200

    DbContext class BookServiceContext + connection string in appsettings + dependency injection for dbContext

---
+++ BookService.WebAPI/Models/BookServiceContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookService.WebAPI.Models
+{
+    public class BookServiceContext : DbContext
+    {
+        public BookServiceContext(DbContextOptions<BookServiceContext> options)
+             : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Singularize table names
+            modelBuilder.Entity<Publisher>().ToTable("Publisher");
+            modelBuilder.Entity<Author>().ToTable("Author");
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- BookService.WebAPI/Startup.cs
+++ BookService.WebAPI/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BookService.WebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace BookService.WebAPI
 {
@@ -25,6 +21,8 @@ namespace BookService.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<BookServiceContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BookService")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
