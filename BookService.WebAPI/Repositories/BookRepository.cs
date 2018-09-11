2ada7244e6cdbd6d835d0b4b7f5d7ee89615442f
wilschok <william.schokkele@howest.be>
2018-09-11 14:54:29 +0200

    BookRepository: List of Books, List of BookBasic DTO

---
+++ BookService.WebAPI/Repositories/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookService.WebAPI.DTO;
+using BookService.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Repositories
+{
+    
+    public class BookRepository
+    {
+        private BookServiceContext db;
+        public BookRepository(BookServiceContext context)
+        {
+            db = context;
+        }
+        public List<Book> List()
+        {
+            // return a list of books with all Book-properties
+            return db.Books.ToList();
+        }
+
+        public List<BookBasic> ListBasic()
+        {
+            // return a list of BookBasic DTO-items (Id and Title only)
+            return db.Books.Select(b => new BookBasic
+            {
+                Id = b.Id,
+                Title = b.Title
+            }).ToList();
+        }
+    }
+
+    
+}
