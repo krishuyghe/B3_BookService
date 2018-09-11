7a7853eb5e83149250de72dac46d01ac13daa21c
wilschok <william.schokkele@howest.be>
2018-09-11 13:56:46 +0200

    remove usings

--- BookService.WebAPI/Models/Publisher.cs
+++ BookService.WebAPI/Models/Publisher.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BookService.WebAPI.Models
+﻿namespace BookService.WebAPI.Models
 {
     public class Publisher
     {
