c62abb2763129904c8ceb5e1ff4d470d22a16b8a
wilschok <william.schokkele@howest.be>
2018-09-11 13:47:09 +0200

    Data Seeding, Migration, Update Database

---
+++ BookService.WebAPI/Migrations/20180911113930_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookService.WebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    ISBN = table.Column<string>(nullable: true),
+                    NumberOfPages = table.Column<int>(nullable: false),
+                    AuthorId = table.Column<int>(nullable: true),
+                    PublisherId = table.Column<int>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Book_Author_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Author",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Book_Publisher_PublisherId",
+                        column: x => x.PublisherId,
+                        principalTable: "Publisher",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Author",
+                columns: new[] { "Id", "BirthDate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1980, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "James", "Sharp" },
+                    { 2, new DateTime(1992, 3, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sophie", "Netty" },
+                    { 3, new DateTime(1996, 8, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Elisa", "Yammy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publisher",
+                columns: new[] { "Id", "Country", "Name" },
+                values: new object[,]
+                {
+                    { 1, "UK", "IT-publishers" },
+                    { 2, "Sweden", "FoodBooks" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "AuthorId", "ISBN", "ImageUrl", "NumberOfPages", "PublisherId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "123456789", "images/book1.jpg", 420, 1, "Learning C#" },
+                    { 2, 2, "45689132", "images/book2.jpg", 360, 1, "Mastering Linq" },
+                    { 3, 1, "15856135", "images/book2.jpg", 360, 1, "Mastering Xamarin" },
+                    { 4, 2, "56789564", "images/book3.jpg", 360, 1, "Exploring ASP.Net Core 2.0" },
+                    { 5, 2, "234546684", "images/book1.jpg", 420, 1, "Unity Game Development" },
+                    { 6, 3, "789456258", "images/book3.jpg", 40, 2, "Cooking is fun" },
+                    { 7, 3, "94521546", "images/book3.jpg", 53, 2, "Secret recipes" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorId",
+                table: "Book",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_PublisherId",
+                table: "Book",
+                column: "PublisherId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Publisher");
+        }
+    }
+}
--- BookService.WebAPI/Models/BookServiceContext.cs
+++ BookService.WebAPI/Models/BookServiceContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace BookService.WebAPI.Models
 {
@@ -12,9 +13,121 @@ namespace BookService.WebAPI.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Singularize table names
-            modelBuilder.Entity<Publisher>().ToTable("Publisher");
-            modelBuilder.Entity<Author>().ToTable("Author");
-            modelBuilder.Entity<Book>().ToTable("Book");
+            modelBuilder.Entity<Publisher>()
+                .ToTable("Publisher")
+                .HasData(
+                 new Publisher
+                 {
+                     Id = 1,
+                     Name = "IT-publishers",
+                     Country = "UK"
+                 },
+                new Publisher
+                {
+                    Id = 2,
+                    Name = "FoodBooks",
+                    Country = "Sweden"
+                }
+                );
+            modelBuilder.Entity<Author>()
+                .ToTable("Author")
+                .HasData(
+                new Author
+                {
+                    Id = 1,
+                    FirstName = "James",
+                    LastName = "Sharp",
+                    BirthDate = new DateTime(1980, 5, 20)
+                },
+                new Author
+                {
+                    Id = 2,
+                    FirstName = "Sophie",
+                    LastName = "Netty",
+                    BirthDate = new DateTime(1992, 3, 4)
+                },
+                new Author
+                {
+                    Id = 3,
+                    FirstName = "Elisa",
+                    LastName = "Yammy",
+                    BirthDate = new DateTime(1996, 8, 12)
+                });
+
+            // using ANONYMOUS CLASS to construct Books in Db (AuthorId and PublisherId are no real properties 
+            modelBuilder.Entity<Book>()
+                .ToTable("Book")
+                .HasData(
+                new
+                {
+                    Id = 1,
+                    ISBN = "123456789",
+                    Title = "Learning C#",
+                    NumberOfPages = 420,
+                    ImageUrl = "images/book1.jpg",
+                    AuthorId = 1,
+                    PublisherId = 1
+                },
+                new
+                {
+                    Id = 2,
+                    ISBN = "45689132",
+                    Title = "Mastering Linq",
+                    NumberOfPages = 360,
+                    ImageUrl = "images/book2.jpg",
+                    AuthorId = 2,
+                    PublisherId = 1
+                },
+                new
+                {
+                    Id = 3,
+                    ISBN = "15856135",
+                    Title = "Mastering Xamarin",
+                    NumberOfPages = 360,
+                    ImageUrl = "images/book2.jpg",
+                    AuthorId = 1,
+                    PublisherId = 1
+                },
+                new
+                {
+                    Id = 4,
+                    ISBN = "56789564",
+                    Title = "Exploring ASP.Net Core 2.0",
+                    NumberOfPages = 360,
+                    ImageUrl = "images/book3.jpg",
+                    AuthorId = 2,
+                    PublisherId = 1
+                },
+                new
+                {
+                    Id = 5,
+                    ISBN = "234546684",
+                    Title = "Unity Game Development",
+                    NumberOfPages = 420,
+                    ImageUrl = "images/book1.jpg",
+                    AuthorId = 2,
+                    PublisherId = 1
+                },
+                new
+                {
+                    Id = 6,
+                    ISBN = "789456258",
+                    Title = "Cooking is fun",
+                    NumberOfPages = 40,
+                    ImageUrl = "images/book3.jpg",
+                    AuthorId = 3,
+                    PublisherId = 2
+                },
+                new
+                {
+                    Id = 7,
+                    ISBN = "94521546",
+                    Title = "Secret recipes",
+                    NumberOfPages = 53,
+                    ImageUrl = "images/book3.jpg",
+                    AuthorId = 3,
+                    PublisherId = 2
+                });
         }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<Author> Authors { get; set; }
