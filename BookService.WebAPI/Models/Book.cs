29f4716ffb01e2826184026896c214135d605fb7
wilschok <william.schokkele@howest.be>
2018-09-12 16:36:42 +0200

    FileName

--- BookService.WebAPI/Models/Book.cs
+++ BookService.WebAPI/Models/Book.cs
@@ -15,6 +15,6 @@ namespace BookService.WebAPI.Models
         public int NumberOfPages { get; set; }
         public Author Author { get; set; }
         public Publisher Publisher { get; set; }
-        public string ImageUrl { get; set; }
+        public string FileName { get; set; }
     }
 }
