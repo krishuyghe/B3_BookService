a785de6f8fa25a49d9482cf5006b127eb5ff4afc
wilschok <william.schokkele@howest.be>
2018-09-15 14:32:44 +0200

    PublisherRepository

---
+++ BookService.WebAPI/Repositories/PublisherRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookService.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class PublisherRepository
+    {
+
+        private BookServiceContext db;
+        public PublisherRepository(BookServiceContext context)
+        {
+            db = context;
+        }
+
+        public List<Publisher> List()
+        {
+            // return a list of books with all Book-properties
+            return db.Publishers.ToList();
+        }
+
+       
+        public Publisher GetById(int id)
+        {
+            return db.Publishers.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
