e22a544b56f68e8326144169f84410dd0fd4eac2
wilschok <william.schokkele@howest.be>
2018-09-15 17:41:47 +0200

    Update, Insert, Delete

--- BookService.WebAPI/Controllers/PublishersController.cs
+++ BookService.WebAPI/Controllers/PublishersController.cs
@@ -1,5 +1,7 @@
-﻿using BookService.WebAPI.Repositories;
+﻿using BookService.WebAPI.Models;
+using BookService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BookService.WebAPI.Controllers
 {
@@ -28,5 +30,63 @@ namespace BookService.WebAPI.Controllers
         {
             return Ok(repository.GetById(id));
         }
+
+        // PUT: api/Publishers/5
+        [HttpPut("{id}")]    
+        public async Task<IActionResult> PutPublisher([FromRoute] int id, [FromBody] Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != publisher.Id)
+            {
+                return BadRequest();
+            }
+
+            Publisher p = await repository.Update(publisher);
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Publishers
+        [HttpPost]
+        public async Task<IActionResult> PostPublisher([FromBody] Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await repository.Add(publisher);
+            
+
+            return CreatedAtAction("GetPublisher", new { id = publisher.Id }, publisher);
+        }
+
+        // DELETE: api/Publishers/3
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublisher([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var publisher = await repository.Delete(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(publisher);
+        }
+
+
     }
 }
--- BookService.WebAPI/Repositories/PublisherRepository.cs
+++ BookService.WebAPI/Repositories/PublisherRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BookService.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,52 @@ namespace BookService.WebAPI.Repositories
         public Publisher GetById(int id)
         {
             return db.Publishers.FirstOrDefault(p => p.Id == id);
+        }
+
+        public async Task<Publisher> Update(Publisher publisher)
+        {
+            try
+            {
+                db.Entry(publisher).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(publisher.Id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return publisher;
+        }
+
+        public async Task<Publisher> Add(Publisher publisher)
+        {
+            db.Publishers.Add(publisher);
+            await db.SaveChangesAsync();
+            return publisher;
+        }
+
+        public async Task<Publisher> Delete(int id)
+        {
+            var publisher = await db.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return null;
+            }
+
+            db.Publishers.Remove(publisher);
+            await db.SaveChangesAsync();
+            return publisher;
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return db.Publishers.Any(e => e.Id == id);
         }
     }
 }
