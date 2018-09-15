51928ff276d9a1124a1e30312e40df031b50393a
wilschok <william.schokkele@howest.be>
2018-09-15 14:24:28 +0200

    AuthorRepository

---
+++ BookService.WebAPI/Repositories/AuthorRepository.cs
@@ -0,0 +1,38 @@
+﻿using BookService.WebAPI.DTO;
+using BookService.WebAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class AuthorRepository
+    {
+        private BookServiceContext db;
+        public AuthorRepository(BookServiceContext context)
+        {
+            db = context;
+        }
+
+        public List<Author> List()
+        {
+            // return a list of books with all Book-properties
+            return db.Authors.ToList();
+        }
+
+        public List<AuthorBasic> GetAuthorBasic()
+        {
+            // return a list of books with all Book-properties
+            return db.Authors.Select(a => new AuthorBasic
+            {
+                Id = a.Id,
+                Name = $"{a.LastName} {a.FirstName}"
+
+            }).ToList();
+        }
+
+        public Author GetById(int id)
+        {
+            return db.Authors.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
