40705a66b9e50184eb21986279939fde3f30fe3e
wilschok <william.schokkele@howest.be>
2018-09-15 15:34:46 +0200

    Book Images

--- BookService.WebAPI/Controllers/BookController.cs
+++ BookService.WebAPI/Controllers/BookController.cs
@@ -7,6 +7,7 @@ using BookService.WebAPI.Models;
 using BookService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using BookService.WebAPI.DTO;
 
 namespace BookService.WebAPI.Controllers
 {
@@ -38,11 +39,30 @@ namespace BookService.WebAPI.Controllers
             return Ok(repository.ListBasic());
         }
 
-        // GET: api/Book/id
+        // GET: api/Book/3
         [HttpGet("{id}")]
         public IActionResult GetBook(int id)
         {
             return Ok(repository.GetById(id));
+        }
+
+        // GET: api/book/imagebyname/book2.jpg
+        [HttpGet]
+        [Route("ImageByName/{filename}")]
+        public IActionResult ImageByFileName(string filename)
+        {
+            var image = Path.Combine(Directory.GetCurrentDirectory(),
+                             "wwwroot", "images", filename);
+            return PhysicalFile(image, "image/jpeg");
+        }
+
+        // GET: api/book/imagebyid/6
+        [HttpGet]
+        [Route("ImageById/{bookid}")]
+        public IActionResult ImageById(int bookid)
+        {
+            BookDetail book = repository.GetById(bookid);
+            return ImageByFileName(book.FileName);
         }
 
     }
