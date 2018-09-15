3385d14d1fce4367183fd4cd41860ca5941a59c5
wilschok <william.schokkele@howest.be>
2018-09-15 14:12:02 +0200

    AuthorBasic

---
+++ BookService.WebAPI/DTO/AuthorBasic.cs
@@ -0,0 +1,8 @@
+﻿namespace BookService.WebAPI.DTO
+{
+    public class AuthorBasic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- BookService.WebAPI/Models/Book.cs
+++ BookService.WebAPI/Models/Book.cs
@@ -8,6 +8,9 @@ namespace BookService.WebAPI.Models
         public string Title { get; set; }
         public string ISBN { get; set; }
         [Display(Name = "#")]
+        public int Year { get; set; }
+
+        public decimal Price { get; set; }
         public int NumberOfPages { get; set; }
         public Author Author { get; set; }
         public Publisher Publisher { get; set; }
