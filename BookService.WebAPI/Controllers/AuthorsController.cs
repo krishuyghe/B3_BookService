25d73a8992690a7944f472afba6cd72d740367fc
wilschok <william.schokkele@howest.be>
2018-09-15 16:41:42 +0200

    PublishersController

--- BookService.WebAPI/Controllers/AuthorsController.cs
+++ BookService.WebAPI/Controllers/AuthorsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BookService.WebAPI.Repositories;
-using Microsoft.AspNetCore.Http;
+﻿using BookService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookService.WebAPI.Controllers
---
+++ BookService.WebAPI/Controllers/PublishersController.cs
@@ -0,0 +1,32 @@
+﻿using BookService.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        PublisherRepository repository;
+
+        public PublishersController(PublisherRepository publisherRepository)
+        {
+            repository = publisherRepository;
+        }
+
+        // GET: api/Publishers
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            return Ok(repository.List());
+        }
+
+
+        // GET: api/Publishers/2
+        [HttpGet("{id}")]
+        public IActionResult GetPublisher(int id)
+        {
+            return Ok(repository.GetById(id));
+        }
+    }
+}
