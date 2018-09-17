18f2935094b1fa7fd1ef32652d49c69dbf3b3ec6
wilschok <william.schokkele@howest.be>
2018-09-17 09:30:02 +0200

    Created Date - business logic

--- BookService.WebAPI/Models/EntityBase.cs
+++ BookService.WebAPI/Models/EntityBase.cs
@@ -6,6 +6,9 @@ namespace BookService.WebAPI.Models
     {
         public int Id { get; set; }
 
+        // in db default value getdate() --> BookServiceContext (object enter db from other ways)
+
+        // program logic: date of creating the object
         private DateTime? created;
         public DateTime? Created {
             get
@@ -13,7 +16,11 @@ namespace BookService.WebAPI.Models
                 return created ?? DateTime.Now;
             }
 
-            set { created = value; }
+            set {
+                if (value != null)
+                    created = value;
+                else created = DateTime.Now;
+            }
         }
     }
 }
