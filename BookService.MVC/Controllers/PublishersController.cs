6a18bb1b191636d81f1ec39d2fae281a48db58d4
wilschok <william.schokkele@howest.be>
2018-09-20 18:20:23 +0200

    Full Crud

--- BookService.MVC/Controllers/PublishersController.cs
+++ BookService.MVC/Controllers/PublishersController.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using BookService.Lib.DTO;
 using BookService.Lib.Models;
 using BookService.MVC.Helpers;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
 
 namespace BookService.MVC.Controllers
 {
     public class PublishersController : Controller
     {
         string baseuri = "http://localhost:53945/api/publishers";
-        private object httpClient;
 
         public IActionResult Index()
         {
@@ -35,27 +31,39 @@ namespace BookService.MVC.Controllers
             return View("Detail",WebApiHelper.GetApiResult<Publisher>(uri));
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Update(Publisher publisher)
+        public IActionResult Insert()
         {
-            string uri = $"{baseuri}/{publisher.Id}";
+            ViewBag.Mode = "Edit";
+            return View("Detail", new Publisher());
+        }
 
-
-            Publisher p = await WebApiHelper.PutCallAPI<Publisher, Publisher>(uri, publisher);
+        [HttpPost]
+        public async Task<IActionResult> Save(Publisher publisher)
+        { 
+            if (publisher.Id != 0)
+            {
+                // update
+                string uri = $"{baseuri}/{publisher.Id}";
+                publisher = await WebApiHelper.PutCallAPI<Publisher, Publisher>(uri, publisher);
+            }
+            else
+            {
+                // insert
+                string uri = $"{baseuri}";
+                publisher = await WebApiHelper.PostCallAPI<Publisher, Publisher>(uri, publisher);
+            }
 
             ViewBag.Mode = "Detail";
             return View("Detail", publisher);
         }
 
-        [HttpPost]
-        public IActionResult Insert(Publisher publisher)
+        public async Task<IActionResult> Delete(int id)
         {
-            string uri = $"{baseuri}/{publisher.Id}";
-
-            // save in db
-
-            ViewBag.Mode = "Detail";
-            return View("Detail", publisher);
+            string uri = $"{baseuri}/{id}";
+            Publisher publisher = await WebApiHelper.DelCallAPI<Publisher>(uri);
+            return RedirectToAction("Index");
         }
+
+
     }
 }
